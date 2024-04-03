7e4b15e00cb32f1e9741700060a2c65534cd461c
Martins <martins.abelis@gmail.com>
2024-04-03 18:59:35 +0000

    Change abc module

---
+++ docs/abc.md
@@ -0,0 +1,13 @@
+# Welcome to My GitHub Pages Site
+
+## Demos
+
+- Demo 1
+
+## Reports
+
+- Report 1
+
+## Documentation
+
+- [index.md](./docs/index.md)
--- week1/module1/abc/src/main.rs
+++ week1/module1/abc/src/main.rs
@@ -1,3 +1,22 @@
+fn function_with_ten_parameters(
+    param1: i32,
+    param2: i32,
+    param3: i32,
+    param4: i32,
+    param5: i32,
+    param6: i32,
+    param7: i32,
+    param8: i32,
+    param9: i32,
+    param10: i32,
+) -> i32 {
+    param1 + param2 + param3 + param4 + param5 + param6 + param7 + param8 + param9 + param10
+}
+
 fn main() {
     println!("Hello, world!");
+    println!(
+        "sum: {:?}",
+        function_with_ten_parameters(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+    );
 }
